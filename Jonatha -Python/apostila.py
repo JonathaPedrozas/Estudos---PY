cbd54ed999944c9d0e12244eb07ce93739f6e2a8
Jonatha da Silva Pedrozas <100812452+JonathaPedrozas@users.noreply.github.com>
2026-02-08 11:25:03 -0300

    Add files via upload

---
+++ Jonatha -Python/apostila.py
@@ -0,0 +1,99 @@
+#tuple = ('jp','yan','matheus','will')
+#list = ['EB112', 'LaFerrari','civic','civicTypeR']
+#dicionario = {'chave': 'valor'}
+#dicionario = {'nome': 'carro', 'lógica': 'true', 'numero': '2', 'outroNumero': '3.5'}
+#print(dicionario)
+#conjunto = {'carro' ,True, 2, 3.5}
+#print(conjunto)
+
+
+
+#print (tuple[0])
+#print(tuple[1])
+#print(list[2])
+#print(list[3])
+
+
+
+#carros = ['EB112', 'LaFerrari','civic','civicTypeR']
+#for carros in carros: #VAI COLOCAR CADA PALAVRA SEPARADA UMA EMBAIXO DA OUTRA
+#print(carros)
+
+
+
+#CONVERTENDO LIST EM TUPLE
+#List = [1,2,3,4,5]
+#print(List[1])
+#Tuple = tuple(List)
+#print(Tuple[3])
+
+
+
+#LIST
+#alunos =['jp', ' will', ' yan', 'vitor','teteu']
+#alunos.sort()#SORTEIA A ORDEM DA LISTA
+#alunos.pop(1)#REMOVE O VALOR SELECIONADO
+#alunos.append('jp3')#ADICIONANDO UM NOVO ELEMENTO
+#alunos.insert(1, 'Jp2')#ADICIONA O VALOR ESCRITO NO LUGAR DETERMINADO
+#print(alunos)
+
+
+
+#DICIONARIO
+#Jonatha = {
+#   'nome': 'Jonatha',
+#   'idade': 16,
+#    'altura': 1.70,
+#    'peso': 60,
+#    'cor': 'pardo'
+#}
+#Jonatha ['peso']= '58,97' #MUDA A CHAVE SELECIONADA
+#print(Jonatha['nome'], Jonatha['idade'], Jonatha['peso'])
+
+#del Jonatha['cor']#DELETAR UMA CHAVE DO DICIONARIO
+
+#print(len(Jonatha))#VER A QUANTIDADE DE CHAVE DENTRO DO DICIONARIO
+
+#print('altura' in Jonatha)#VERIFICAR SE ESSA CHAVE EXISTE DENTRO DO DICIONARIO
+
+#for x in Jonatha:
+ #print(x) #LER AS CHAVES DENTRO DO DICIONARIO (TEM QUE TER UM ESPAÇO DO PRINT ENTRE A BORDA DA PAG)
+
+#for x in Jonatha:
+    #print(x+': '+Jonatha[x])#MOSTRA A CHAVE E VALOR
+
+#print(Jonatha.get('sobrenome', 'essa info n conta'))#LEVA UMA INFORMAÇÃO AO USUARIO SE A CHAVE EXISTE OU NÃO
+#print(Jonatha.get('status','essa info n conta'))
+
+#Jonatha.clear()#RETIRA TUDO DE DENTRO DO DICIONARIO
+#print(Jonatha)
+
+
+
+#A programação estruturada é uma abordagem que utiliza sequência, seleção (decisão) e repetição (loops) como principais estruturas de controle.
+#Basicamente é tipo a estrutura do VisualG e do HTML, 
+#Inicio
+#// Declaração de variáveis
+#inteiro a, b, soma
+#// Entrada de dados
+#ler(a)
+#ler(b)
+#// Processamento
+#soma = a + b
+#// Saída de dados
+#escrever("A soma de ", a, " e ", b, " é ", soma)
+#Fim
+
+#idade = int(input('digite sua idade: '))
+#if idade >= 18:
+ #print('Você é maior de idade')
+#else:
+ #print('Você é menor de idade')
+
+tamanho = float(input('digite sua altura: '))
+if tamanho > 1.70:
+ print('você é alto.')
+elif tamanho == 1.70:
+ print('você é alto.')
+else:
+ print('você é baixo.')
